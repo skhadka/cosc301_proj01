d49c1575156edf6cc9b3bebeb78117695da5a29a
cosc301 <cosc301@csvm.(none)>
2013-09-22 06:39:38 -0400

    Carrie Changes

--- list.c
+++ list.c
@@ -4,3 +4,23 @@
 
 /* your list function definitions */
 
+struct node {
+	int integer;
+	struct node *next;
+};
+
+void list_insert(int num, struct node **head) {
+	struct node * newnode = malloc(sizeof(Struct node));
+	newnode->integer = num;
+	newnode->next = *head;
+	*head = newnode;
+}
+
+void list_print(struct node **head) {
+	struct node * current = *head;
+	while(current!=NULL) {
+		printf("%d", current->integer);
+		current = current->next;
+	}
+}
+
--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*
  *
- * Shreeya Khadka and Carrie Burgess
+ * author name, date, and other info here
  *
  */
 
@@ -9,6 +9,8 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 
 #include "list.h"
 
@@ -17,16 +19,50 @@ void usage(char *program) {
     exit(1);
 }
 
+void file_to_LL(FILE *datafile) {  //added this function
+	//question from Carrie: do we want the pointer?
+    //tokenize everything in file
+    //check if int
+    //if int, add to linked list
+    char * items;
+	const char * spc = " \n\t";
+	char* tmp;
+	char* word;
+	int length;
+	int i;
+	int check;
+	int addToList;
+	struct node * head = malloc(sizeof(struct node));
+	//create first node in memory
+	while(fscanf(datafile, "%s", items) != EOF) {
+		for(word=strtok_r(s,spc,&tmp); word!=NULL; word=strtok_r(NULL, spc,&tmp)) {
+			length = strlen(word);
+			i=0;
+			check=0;
+			for(;i<length;i++) {
+				if(isdigit(word[i]==0) {
+					check=1; //if part of word/string is not a digit, not happy
+					break;
+				}
+			}
+			if(check==0) { //means everything is a number (in the word....)
+				addToList = atoi(word);
+				list_insert(addToList, &head);
+				//this calls to list.c
+			}
+		}
+	}
+}
+
 
 int main(int argc, char **argv) {
     FILE *datafile = NULL;
-
     /* find out how we got invoked and deal with it */
     switch (argc) {
         case 1:
-            /* only one program argument (the program name) */ 
+            /* only one program argument (the program name) */
             /* just equate stdin with our datafile */
-            datafile = stdin;        
+            datafile = stdin;
             break;
 
         case 2:
@@ -40,8 +76,11 @@ int main(int argc, char **argv) {
             break;
 
         default:
+            perror("Please enter correct number of arguments."); //Carrie added
             /* more than two arguments?  throw hands up in resignation */
             usage(argv[0]);
+		//add call to file_to_LL?
+		//add getrusage thing?
     }
 
     /* 
