507a252842b929269da1b51aacb3f592a826f6b6
cosc301 <cosc301@csvm.(none)>
2013-09-23 21:23:57 -0400

    prints a sorted linked list of integers

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 /*
- *
- * author name, date, and other info here
- *
+  Shreeya Khadka and Carrie Burgess
+  Project 1: 09/25/13
  */
 
 #include <stdio.h>
@@ -19,12 +18,11 @@ void usage(char *program) {
     exit(1);
 }
 
-void file_to_LL(FILE *datafile) {  //added this function
-	//question from Carrie: do we want the pointer?
+void file_to_LL(FILE *datafile) {  
     //tokenize everything in file
     //check if int
     //if int, add to linked list
-    char * items;
+    char items[126];
 	const char * spc = " \n\t";
 	char* tmp;
 	char* word;
@@ -32,26 +30,29 @@ void file_to_LL(FILE *datafile) {  //added this function
 	int i;
 	int check;
 	int addToList;
-	struct node * head = malloc(sizeof(struct node));
+	struct node * head = (struct node *) malloc(sizeof(struct node));
 	//create first node in memory
-	while(fscanf(datafile, "%s", items) != EOF) {
-		for(word=strtok_r(s,spc,&tmp); word!=NULL; word=strtok_r(NULL, spc,&tmp)) {
+	while(fgets(items,126,datafile)!= NULL) {
+	        //printf("Reading line, word: %s\n",items);
+		for(word=strtok_r(items,spc,&tmp); word!=NULL; word=strtok_r(NULL,spc,&tmp)) {
 			length = strlen(word);
 			i=0;
-			check=0;
+			check=1;
+			if (word[0]=='-') i = 1; //taking care of negative integers
 			for(;i<length;i++) {
-				if(isdigit(word[i]==0) {
-					check=1; //if part of word/string is not a digit, not happy
+				if(!isdigit((int)word[i])) {
+					check=0; //character is not a digit
 					break;
 				}
 			}
-			if(check==0) { //means everything is a number (in the word....)
+			if(check) { //check=1 -> all characters are digits
 				addToList = atoi(word);
 				list_insert(addToList, &head);
 				//this calls to list.c
 			}
 		}
 	}
+	list_print(&head);            
 }
 
 
@@ -62,25 +63,32 @@ int main(int argc, char **argv) {
         case 1:
             /* only one program argument (the program name) */
             /* just equate stdin with our datafile */
+	    //printf("case1: reading stdin\n");
             datafile = stdin;
+	    file_to_LL(datafile); //read stdin and put in in LL
             break;
 
         case 2:
+	    //printf("case2: opening file\n");
             /* two arguments: program name and input file */
             /* open the file, assign to datafile */
             datafile = fopen(argv[1], "r");
             if (datafile == NULL) {
                 printf("Unable to open file %s: %s\n", argv[1], strerror(errno));
                 exit(-1);
-            }
+            }	
+	    file_to_LL(datafile); //read datafile and put in in LL
             break;
 
         default:
             perror("Please enter correct number of arguments."); //Carrie added
             /* more than two arguments?  throw hands up in resignation */
-            usage(argv[0]);
+
+		usage(argv[0]);
 		//add call to file_to_LL?
 		//add getrusage thing?
+
+
     }
 
     /* 
